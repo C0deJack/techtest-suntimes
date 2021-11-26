a131972fd064dc90ade7aa1c5b2a5b7ef3041706
Jack <Jackwoodpearce@gmail.com>
2021-11-26 16:45:52 +0000

    Replace API for sunrise-sunset.org as times on openweathermap are wrong

--- src/Weather.js
+++ src/Weather.js
@@ -2,21 +2,14 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import getData from './helpers/getData';
 import debounce from './helpers/debounce';
-import formatDate from './helpers/formatDate';
 import appSettings from './appconfig.json';
 const settings = appSettings;
-
-// Temp
-// import { exampleData } from './helpers/exampleWeatherData';
 
 const StyledWeather = styled.div`
     padding: 0.5rem;
 `;
 
 export default function Weather() {
-    // const cityName = 'London,uk';
-    // const url = `${settings.urls.api.weatherByCityName}${cityName}${settings.keys.weatherApiKey}`;
-
     const [location, setLocation] = useState({});
 
     useEffect(() => {
@@ -38,20 +31,16 @@ export default function Weather() {
 
     const handleError = err => console.log(err);
 
-    // const lat = '52.2';
-    // const lon = '0.12';
-
-    const url = `${settings.urls.api.weatherByLatLon}lat=${location.lat}&lon=${location.lon}${settings.keys.weatherApiKey}`;
+    const url = `${settings.urls.api.sunTimesByLatLon}lat=${location.lat}&lng=${location.lon}`;
 
     const [weather, setWeather] = useState('');
-    // const [weather] = useState(exampleData);
 
     useEffect(() => {
         debounce(
             getData(url)
                 .then(data => {
                     console.log(data);
-                    return setWeather(data);
+                    return setWeather(data.results);
                 })
                 .catch(() => setWeather('')),
             1000,
@@ -64,8 +53,8 @@ export default function Weather() {
             {weather ? (
                 <StyledWeather title='sunrise and sunset'>
                     <h2>{weather.name}</h2>
-                    <div>{`${formatDate(weather.sys.sunrise)}am`}</div>
-                    <div>{`${formatDate(weather.sys.sunset)}pm`}</div>
+                    <div>{weather.sunrise}</div>
+                    <div>{weather.sunset}</div>
                 </StyledWeather>
             ) : (
                 <div>Loading</div>
