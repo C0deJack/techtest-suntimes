3248da4389aa6e4e11e78a94fcdd4a452b0d7221
Jack <jackwoodpearce@gmail.com>
2021-12-09 21:39:16 +0000

    Format time

--- src/Time.tsx
+++ src/Time.tsx
@@ -31,10 +31,16 @@ interface Props {
 }
 
 const Time = ({ title, icon, time }: Props): JSX.Element => {
+    const trimmedTime = time.slice(0, -6);
+    const amOrPm = time.slice(-2);
+
     return (
         <StyledSection>
             <img className='icon' src={icon} />
-            <time>{time}</time>
+            <time>
+                {trimmedTime}
+                {amOrPm}
+            </time>
             <h2>{title}</h2>
         </StyledSection>
     );
