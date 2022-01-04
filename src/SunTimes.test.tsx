4a557c3111d5ed291b7f6ae51a641156440dd18e
Jack Wood-Pearce <jackwoodpearce@gmail.com>
2022-01-04 12:01:16 +0000

    Update

--- src/SunTimes.test.tsx
+++ src/SunTimes.test.tsx
@@ -1,7 +1,32 @@
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import getLocation from './getLocation';
 import SunTimes from './SunTimes';
+// import { shallow } from 'enzyme';
 
-test('SunTimes renders correctly', () => {
-    const sunTimes = renderer.create(<SunTimes />).toJSON();
-    expect(sunTimes).toMatchSnapshot();
+// describe('<SunTimes />', () => {
+//     it('Calls getLocation on mount', () => {
+//         renderer.act(<SunTimes />);
+//     });
+// });
+
+import { mount } from 'enzyme';
+
+// describe('<SunTimes />', () => {
+//     test('should call getLocation on mount', () => {
+//         const spy = jest.spyOn(SunTimes.prototype, 'getLocation');
+//         mount(<SunTimes />);
+//         expect(spy).toHaveBeenCalledTimes(1);
+//     });
+// });
+
+import { act } from 'react-dom/test-utils';
+
+test('setInitialData fn is called in useEffect hook in the provider', async () => {
+    let debug;
+    await act(async () => {
+        debug = render(<SunTimes />).debug;
+    });
+
+    expect(getLocation).toHaveBeenCalled();
 });
